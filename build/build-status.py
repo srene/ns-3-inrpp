ef0297696c724c2bbda32e947d9758daa93015b6
srene <s.rene@ucl.ac.uk>
2015-08-05 00:32:01 +0200

    build removed

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['scratch/ns3.23-inrpp2-debug', 'scratch/subdir/ns3.23-subdir-debug', 'scratch/ns3.23-scratch-simulator-debug', 'scratch/ns3.23-inrpp-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
