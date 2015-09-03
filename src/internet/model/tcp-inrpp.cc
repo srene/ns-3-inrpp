e24f74a435725cdae09adc97ad9967d6d107407b
srene <s.rene@ucl.ac.uk>
2015-09-03 09:48:59 +0200

    inrpp commit

---
+++ src/internet/model/tcp-inrpp.cc
@@ -0,0 +1,276 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2010 Adrian Sai-wah Tam
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Adrian Sai-wah Tam <adrian.sw.tam@gmail.com>
+ */
+
+#define NS_LOG_APPEND_CONTEXT \
+  if (m_node) { std::clog << Simulator::Now ().GetSeconds () << " [node " << m_node->GetId () << "] "; }
+
+#include "tcp-inrpp.h"
+#include "ns3/log.h"
+#include "ns3/trace-source-accessor.h"
+#include "ns3/simulator.h"
+#include "ns3/abort.h"
+#include "ns3/node.h"
+#include "ns3/packet.h"
+#include "ns3/inrpp-backp-tag.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("TcpInrpp");
+
+NS_OBJECT_ENSURE_REGISTERED (TcpInrpp);
+
+TypeId
+TcpInrpp::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::TcpInrpp")
+    .SetParent<TcpSocketBase> ()
+    .SetGroupName ("Internet")
+    .AddConstructor<TcpInrpp> ()
+    .AddAttribute ("ReTxThreshold", "Threshold for fast retransmit",
+                    UintegerValue (3),
+                    MakeUintegerAccessor (&TcpInrpp::m_retxThresh),
+                    MakeUintegerChecker<uint32_t> ())
+    .AddAttribute ("LimitedTransmit", "Enable limited transmit",
+                   BooleanValue (false),
+                   MakeBooleanAccessor (&TcpInrpp::m_limitedTx),
+                   MakeBooleanChecker ())
+    .AddTraceSource ("CongestionWindow",
+                     "The TCP connection's congestion window",
+                     MakeTraceSourceAccessor (&TcpInrpp::m_cWnd),
+                     "ns3::TracedValue::Uint32Callback")
+    .AddTraceSource ("SlowStartThreshold",
+                     "TCP slow start threshold (bytes)",
+                     MakeTraceSourceAccessor (&TcpInrpp::m_ssThresh),
+                     "ns3::TracedValue::Uint32Callback")
+ ;
+  return tid;
+}
+
+TcpInrpp::TcpInrpp (void)
+  : m_retxThresh (3), // mute valgrind, actual value set by the attribute system
+    m_inFastRec (false),
+    m_limitedTx (false) // mute valgrind, actual value set by the attribute system
+{
+  NS_LOG_FUNCTION (this);
+}
+
+TcpInrpp::TcpInrpp (const TcpInrpp& sock)
+  : TcpSocketBase (sock),
+    m_cWnd (sock.m_cWnd),
+    m_ssThresh (sock.m_ssThresh),
+    m_initialCWnd (sock.m_initialCWnd),
+    m_initialSsThresh (sock.m_initialSsThresh),
+    m_retxThresh (sock.m_retxThresh),
+    m_inFastRec (false),
+    m_limitedTx (sock.m_limitedTx)
+{
+  NS_LOG_FUNCTION (this);
+  NS_LOG_LOGIC ("Invoked the copy constructor");
+}
+
+TcpInrpp::~TcpInrpp (void)
+{
+}
+
+/* We initialize m_cWnd from this function, after attributes initialized */
+int
+TcpInrpp::Listen (void)
+{
+  NS_LOG_FUNCTION (this);
+  InitializeCwnd ();
+  return TcpSocketBase::Listen ();
+}
+
+/* We initialize m_cWnd from this function, after attributes initialized */
+int
+TcpInrpp::Connect (const Address & address)
+{
+  NS_LOG_FUNCTION (this << address);
+  InitializeCwnd ();
+  return TcpSocketBase::Connect (address);
+}
+
+/* Limit the size of in-flight data by cwnd and receiver's rxwin */
+uint32_t
+TcpInrpp::Window (void)
+{
+  NS_LOG_FUNCTION (this);
+  return std::min (m_rWnd.Get (), m_cWnd.Get ());
+}
+
+Ptr<TcpSocketBase>
+TcpInrpp::Fork (void)
+{
+  return CopyObject<TcpInrpp> (this);
+}
+
+/* New ACK (up to seqnum seq) received. Increase cwnd and call TcpSocketBase::NewAck() */
+void
+TcpInrpp::NewAck (const SequenceNumber32& seq)
+{
+  NS_LOG_FUNCTION (this << seq);
+  NS_LOG_LOGIC ("TcpInrpp received ACK for seq " << seq <<
+                " cwnd " << m_cWnd <<
+                " ssthresh " << m_ssThresh);
+
+  // Check for exit condition of fast recovery
+  if (m_inFastRec && seq < m_recover)
+    { // Partial ACK, partial window deflation (RFC2582 sec.3 bullet #5 paragraph 3)
+      m_cWnd += m_segmentSize - (seq - m_txBuffer->HeadSequence ());
+      NS_LOG_INFO ("Partial ACK for seq " << seq << " in fast recovery: cwnd set to " << m_cWnd);
+      m_txBuffer->DiscardUpTo(seq);  //Bug 1850:  retransmit before newack
+      DoRetransmit (); // Assume the next seq is lost. Retransmit lost packet
+      TcpSocketBase::NewAck (seq); // update m_nextTxSequence and send new data if allowed by window
+      return;
+    }
+  else if (m_inFastRec && seq >= m_recover)
+    { // Full ACK (RFC2582 sec.3 bullet #5 paragraph 2, option 1)
+      m_cWnd = std::min (m_ssThresh.Get (), BytesInFlight () + m_segmentSize);
+      m_inFastRec = false;
+      NS_LOG_INFO ("Received full ACK for seq " << seq <<". Leaving fast recovery with cwnd set to " << m_cWnd);
+    }
+
+  // Increase of cwnd based on current phase (slow start or congestion avoidance)
+  if (m_cWnd < m_ssThresh)
+    { // Slow start mode, add one segSize to cWnd. Default m_ssThresh is 65535. (RFC2001, sec.1)
+      m_cWnd += m_segmentSize;
+      NS_LOG_INFO ("In SlowStart, ACK of seq " << seq << "; update cwnd to " << m_cWnd << "; ssthresh " << m_ssThresh);
+    }
+  else
+    { // Congestion avoidance mode, increase by (segSize*segSize)/cwnd. (RFC2581, sec.3.1)
+      // To increase cwnd for one segSize per RTT, it should be (ackBytes*segSize)/cwnd
+      double adder = static_cast<double> (m_segmentSize * m_segmentSize) / m_cWnd.Get ();
+      adder = std::max (1.0, adder);
+      m_cWnd += static_cast<uint32_t> (adder);
+      NS_LOG_INFO ("In CongAvoid, updated to cwnd " << m_cWnd << " ssthresh " << m_ssThresh);
+    }
+
+  // Complete newAck processing
+  TcpSocketBase::NewAck (seq);
+}
+
+/* Cut cwnd and enter fast recovery mode upon triple dupack */
+void
+TcpInrpp::DupAck (const TcpHeader& t, uint32_t count)
+{
+  NS_LOG_FUNCTION (this << count);
+
+}
+
+/* Retransmit timeout */
+void
+TcpInrpp::Retransmit (void)
+{
+  NS_LOG_FUNCTION (this);
+  NS_LOG_LOGIC (this << " ReTxTimeout Expired at time " << Simulator::Now ().GetSeconds ());
+  m_inFastRec = false;
+
+  // If erroneous timeout in closed/timed-wait state, just return
+  if (m_state == CLOSED || m_state == TIME_WAIT) return;
+  // If all data are received (non-closing socket and nothing to send), just return
+  if (m_state <= ESTABLISHED && m_txBuffer->HeadSequence () >= m_highTxMark) return;
+
+  // According to RFC2581 sec.3.1, upon RTO, ssthresh is set to half of flight
+  // size and cwnd is set to 1*MSS, then the lost packet is retransmitted and
+  // TCP back to slow start
+  //m_ssThresh = std::max (2 * m_segmentSize, BytesInFlight () / 2);
+  //m_cWnd = m_segmentSize;
+  m_nextTxSequence = m_txBuffer->HeadSequence (); // Restart from highest Ack
+  NS_LOG_INFO ("RTO. Reset cwnd to " << m_cWnd <<
+               ", ssthresh to " << m_ssThresh << ", restart from seqnum " << m_nextTxSequence);
+  DoRetransmit ();                          // Retransmit the packet
+}
+
+void
+TcpInrpp::SetSegSize (uint32_t size)
+{
+  NS_ABORT_MSG_UNLESS (m_state == CLOSED, "TcpInrpp::SetSegSize() cannot change segment size after connection started.");
+  m_segmentSize = size;
+}
+
+void
+TcpInrpp::SetInitialSSThresh (uint32_t threshold)
+{
+  NS_ABORT_MSG_UNLESS (m_state == CLOSED, "TcpInrpp::SetSSThresh() cannot change initial ssThresh after connection started.");
+  m_initialSsThresh = threshold;
+}
+
+uint32_t
+TcpInrpp::GetInitialSSThresh (void) const
+{
+  return m_initialSsThresh;
+}
+
+void
+TcpInrpp::SetInitialCwnd (uint32_t cwnd)
+{
+  NS_ABORT_MSG_UNLESS (m_state == CLOSED, "TcpInrpp::SetInitialCwnd() cannot change initial cwnd after connection started.");
+  m_initialCWnd = cwnd;
+}
+
+uint32_t
+TcpInrpp::GetInitialCwnd (void) const
+{
+  return m_initialCWnd;
+}
+
+void 
+TcpInrpp::InitializeCwnd (void)
+{
+  /*
+   * Initialize congestion window, default to 1 MSS (RFC2001, sec.1) and must
+   * not be larger than 2 MSS (RFC2581, sec.3.1). Both m_initiaCWnd and
+   * m_segmentSize are set by the attribute system in ns3::TcpSocket.
+   */
+  m_cWnd = m_initialCWnd * m_segmentSize;
+  m_ssThresh = m_initialSsThresh;
+}
+
+void
+TcpInrpp::ScaleSsThresh (uint8_t scaleFactor)
+{
+  m_ssThresh <<= scaleFactor;
+}
+
+void
+TcpInrpp::ReceivedAck (Ptr<Packet> packet, const TcpHeader& tcpHeader)
+{
+	NS_LOG_FUNCTION(this);
+	if(m_lastRtt.Get().GetSeconds()>0)
+	{
+		double rate = std::min (m_rWnd.Get (), m_cWnd.Get ())/m_lastRtt.Get().GetSeconds();
+		NS_LOG_LOGIC("Tcp inrpp rate " << rate);
+
+	}
+	 InrppBackpressureTag backpTagCopy;
+	 if(packet->PeekPacketTag (backpTagCopy))
+	 {
+	  NS_LOG_LOGIC("Backpressure tag " << (uint32_t)backpTagCopy.GetFlag() << " " << backpTagCopy.GetNonce() << " " << backpTagCopy.GetDeltaRate());
+	  NS_LOG_LOGIC("Cwnd halve by " <<m_cWnd << " " << (backpTagCopy.GetDeltaRate()/8)*m_lastRtt.Get().GetSeconds());
+	  if(m_cWnd>((backpTagCopy.GetDeltaRate()/8)*m_lastRtt.Get().GetSeconds()))
+		  m_cWnd = m_cWnd - ((backpTagCopy.GetDeltaRate()/8)*m_lastRtt.Get().GetSeconds());
+	  else
+		  m_cWnd = m_initialCWnd * m_segmentSize;
+	 }
+	TcpSocketBase::ReceivedAck (packet,tcpHeader);
+}
+
+
+} // namespace ns3
